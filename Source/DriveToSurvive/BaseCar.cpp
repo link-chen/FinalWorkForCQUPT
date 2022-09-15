83321e822e0c317faa4a1e0c096f59e559023f04
link-chen <1533842603@qq.com>
2022-09-15 09:15:31 +0800

    Create BaseCar and BaseMap

--- Source/DriveToSurvive/BaseCar.cpp
+++ Source/DriveToSurvive/BaseCar.cpp
@@ -2,4 +2,79 @@
 
 
 #include "BaseCar.h"
+#include "WheeledVehicleMovementComponent.h"
+#include "Components/AudioComponent.h"
+
+ABaseCar::ABaseCar()
+{
+	ExternalCamera=CreateDefaultSubobject<UCameraComponent>(TEXT("ExternalCamera"));
+	InternalCamera=CreateDefaultSubobject<UCameraComponent>(TEXT("InternalCamera"));
+	SpringArm=CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	ExternalCamera->SetupAttachment(SpringArm);
+	InternalCamera->SetupAttachment(RootComponent);
+	Audio=CreateDefaultSubobject<UAudioComponent>(TEXT("AudiComponent"));
+}
+void ABaseCar::BeginPlay()
+{
+	Super::BeginPlay();
+	InternalCamera->Deactivate();
+}
+void ABaseCar::Tick(float DeltaSeconds)
+{
+	
+}
+
+void ABaseCar::MoveForward(float Value)
+{
+	if(GetVehicleMovementComponent()->GetForwardSpeed()<MaxSpeed)
+	GetVehicleMovementComponent()->SetThrottleInput(Value*UpRate);
+	PlayEngineSound();
+}
+
+void ABaseCar::MoveRight(float Value)
+{
+	GetVehicleMovementComponent()->SetSteeringInput(Value);
+}
+
+void ABaseCar::Brake()
+{
+	GetVehicleMovementComponent()->SetHandbrakeInput(true);
+}
+void ABaseCar::CancleBrake()
+{
+	GetVehicleMovementComponent()->SetHandbrakeInput(false);
+}
+
+void ABaseCar::PlayEngineSound()
+{
+	Audio->PitchMultiplier= 0.25f+ (FMath::Abs(GetVehicleMovement()->GetForwardSpeed())*0.045f - 0) / (MaxSpeed - 0)*0.95f;
+	if(!Audio->IsPlaying())
+	{
+		Audio->Play();
+	}
+}
+void ABaseCar::ChangeCamera()
+{
+	if(bExternal)
+	{
+		ExternalCamera->Deactivate();
+		InternalCamera->Activate();
+		bExternal=false;
+	}else
+	{
+		ExternalCamera->Activate();
+		InternalCamera->Deactivate();
+		bExternal=true;
+	}
+}
+
+void ABaseCar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	PlayerInputComponent->BindAction("Brake",IE_Pressed,this,&ABaseCar::Brake);
+	PlayerInputComponent->BindAction("Brake",IE_Released,this,&ABaseCar::CancleBrake);
+	PlayerInputComponent->BindAction("ChangeCamera",IE_Pressed,this,&ABaseCar::ChangeCamera);
+	PlayerInputComponent->BindAxis("MoveForward",this,&ABaseCar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight",this,&ABaseCar::MoveRight);
+}
 
--- Source/DriveToSurvive/BaseCar.h
+++ Source/DriveToSurvive/BaseCar.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "WheeledVehicle.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Sound/SoundCue.h"
 #include "BaseCar.generated.h"
 
 /**
@@ -13,5 +16,36 @@ UCLASS()
 class DRIVETOSURVIVE_API ABaseCar : public AWheeledVehicle
 {
 	GENERATED_BODY()
+
 	
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* ExternalCamera;
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* InternalCamera;
+	UPROPERTY(EditAnywhere)
+	USpringArmComponent* SpringArm;
+	UPROPERTY(EditAnywhere)
+	UAudioComponent* Audio;
+	UPROPERTY(EditAnywhere)
+	float MaxSpeed;
+	UPROPERTY(EditAnywhere)
+	float UpRate;
+	bool bExternal=true;
+	
+
+	
+	UPROPERTY(EditAnywhere)
+	TArray<USoundCue*> SoundArray;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Brake();
+	void CancleBrake();
+	void PlayEngineSound();
+	void ChangeCamera();
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+public:
+	bool bCanUseDRS;
+	ABaseCar();
 };
